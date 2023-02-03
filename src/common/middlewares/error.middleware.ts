b2acc91b86b5542adcd100ea2ded193dc9c1af79
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2023-02-03 16:53:42 +1000

    refactor: :recycle: change middleware response and reqeust types

--- src/common/middlewares/error.middleware.ts
+++ src/common/middlewares/error.middleware.ts
@@ -1,13 +1,15 @@
 import HttpException from '../exceptions/http.exception';
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction } from 'express';
+import BaseRequest from '../requests/base.request';
+import { BaseResponse } from '../responses/base.response';
 
-const errorMiddleware = async (error: Error, _request: Request, response: Response, _next: NextFunction) => {
+const errorMiddleware = async (error: Error, _request: BaseRequest, response: BaseResponse, _next: NextFunction) => {
   if (error instanceof HttpException) {
     const status: number = error.status || 500;
     const message = error.message || 'Internal Server Error';
-    return response.status(status).send({ status, message });
+    return response.status(status).send({ statusCode: status, message });
   }
-  return response.status(500).send({ message: 'Internal Server Error' });
+  return response.status(500).send({ statusCode: 500, message: 'Internal Server Error' });
 };
 
 export default errorMiddleware;
--- src/common/middlewares/notfound.middleware.ts
+++ src/common/middlewares/notfound.middleware.ts
@@ -1,12 +1,14 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction } from 'express';
+import BaseRequest from '../requests/base.request';
+import NotFoundResponse from '../responses/notFound.response';
 
-const NotFoundMiddleware = async (req: Request, res: Response, _next: NextFunction) => {
-  const status = 404;
+const NotFoundMiddleware = async (req: BaseRequest, res: NotFoundResponse, _next: NextFunction) => {
+  const statusCode = 404;
   const message = 'Route Not Found';
   const path: string = req.path;
   const method: string = req.method;
 
-  return res.status(status).send({ status, message, path, method });
+  return res.status(statusCode).send({ statusCode, message, path, method });
 };
 
 export default NotFoundMiddleware;
